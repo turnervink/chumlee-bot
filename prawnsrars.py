299f2cb660e5d33be551332bb4a1b67afb4b384c
Turner Vink <turner@turnervink.com>
2017-07-24 10:59:09 -0700

    Add ytp quotes file

---
+++ prawnsrars.py
@@ -0,0 +1,18 @@
+ytpquotes = [
+    "I'm an expert in Western Alalia.",
+    "You don't wanna mess up this circle of beauty.",
+    "On this episode of Soda Stars...",
+    "That's not my tattoo set.",
+    "I have a cs188 rifle.",
+    "I'm Garrison, and this is my poncho.",
+    "Everything in here has a story. And a price. And a pawn. And an old man.",
+    "I work here with my old mom, and my son Big Sus.",
+    "The people at the auction house assured me that this was an auction.",
+    "What I love about Western Alalia is the vulture."
+    "Well it's definitely a saddle.",
+    "Well I'm sure it's still a saddle, even though we couldn't get that verified.",
+    "My father-in-law used to threaten me when I was dating his daughter, and I told him the first thing I was gonna do was sell her.",
+    "That's why they were called 'That One Gun'",
+    "Basically this wasn't a muzzle loader where you had to put your powder in your ball in put your powder in your ball in put your powder in your ball put your powder in your ball put your powder in your ball...",
+    "This gun is the real deal. Now all I have to do is test it :boom:"
+]
