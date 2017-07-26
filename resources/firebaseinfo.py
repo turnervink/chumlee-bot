61d03af4873c221ca2a3ab7a151e9e4345f3924b
Turner Vink <turner@turnervink.com>
2017-07-26 12:40:27 -0700

    Add key info to env variables

--- resources/firebaseinfo.py
+++ resources/firebaseinfo.py
@@ -1,6 +1,6 @@
 import pyrebase
 import requests
-import re
+import os
 
 url = "https://api.heroku.com/apps/chumlee-bot/config-vars"
 headers = {
@@ -18,10 +18,10 @@ config = {
   "serviceAccount": {
     "type": "service_account",
     "project_id": "chumlee-bot",
-    "private_key_id": result["private_key_id"],
-    "private_key": result["private_key"].replace('\\n', '\n'),
-    "client_email": result["client_email"],
-    "client_id": result["client_id"],
+    "private_key_id": os.environ["private_key_id"],
+    "private_key": os.environ["private_key"].replace('\\n', '\n'),
+    "client_email": os.environ["client_email"],
+    "client_id": os.environ["client_id"],
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://accounts.google.com/o/oauth2/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
