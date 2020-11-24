20ef92de59d7157616c033d4e1b851307dbcbb87
Turner Vink <turner.vink@gmail.com>
2020-11-23 20:44:57 -0800

    Hopefully fix Chumlee emoji

--- util/emoji.py
+++ util/emoji.py
@@ -1,4 +1,4 @@
 CHUMCOIN = '<:chumcoin:337841443907305473>'
-CHUMLEE = '<:chumlee:779856947082952704>'
+CHUMLEE = '<:chumlee:337842115931537408>'
 NO_ENTRY = ':no_entry_sign:'
 ARROW_RIGHT = ':arrow_right:'
