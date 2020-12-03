55c15ff9360888292ee34779fd2cc8b265325455
Turner Vink <turner.vink@gmail.com>
2020-12-02 20:44:29 -0800

    Added channel binding

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
+import discord
 from discord.ext import commands
 
 from command.error.errors import InvalidChannelError
 from util import log
+from util.database import guild_actions
 
 import os
 
@@ -11,15 +13,26 @@ bot = commands.Bot(command_prefix=".")
 
 
 @bot.check
-async def is_in_valid_channel(ctx: commands.Context):
-    if ctx.command.name == "help" or ctx.channel.name in ALLOWED_CHANNELS:
+async def is_in_allowed_channel(ctx: commands.Context):
+    if ctx.command.name in ["allowchannel", "disallowchannel", "allowedchannels"]:
+        return True
+
+    allowed_channels = list(guild_actions.get_allowed_channels(ctx.guild.id).keys())
+    if str(ctx.channel.id) in allowed_channels:
         return True
     else:
         raise InvalidChannelError(ctx.message.author)
 
 
+@bot.check
+async def is_not_dm(ctx: commands.Context):
+    return not isinstance(ctx.message.channel, discord.DMChannel)
+
+
 @bot.event
 async def on_ready():
+    bot.load_extension("util.status")
+    bot.load_extension("util.channel_bind")
     bot.load_extension("command.error.error_handlers")
     bot.load_extension("command.user.help")
     bot.load_extension("command.user.registration")
@@ -28,7 +41,6 @@ async def on_ready():
     bot.load_extension("command.pawnshop.transaction")
     bot.load_extension("command.pawnshop.medal")
     bot.load_extension("command.pawnshop.lotto")
-    bot.load_extension("util.status")
 
     logger = log.setup_logger("chumlee-bot")
     logger.info(f"Bot ready! Logged in as {bot.user.name} - ID: {bot.user.id}")
---
+++ util/channel_bind.py
@@ -0,0 +1,36 @@
+from discord.ext import commands
+
+from util.database import guild_actions
+
+
+class ChannelBind(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="allowchannel", description="Allow the bot to be used in a channel")
+    async def allow_channel(self, ctx: commands.Context):
+        guild_actions.add_allowed_channel(ctx.guild.id, ctx.message.channel.id)
+        await ctx.send(f"Added {ctx.channel.mention} to the list of allowed channels")
+
+    @commands.command(name="disallowchannel", description="Disallow the bot from being used in a channel")
+    async def disallow_channel(self, ctx: commands.Context):
+        guild_actions.remove_allowed_channel(ctx.guild.id, ctx.message.channel.id)
+        await ctx.send(f"Removed {ctx.channel.mention} from the list of allowed channels4")
+
+    @commands.command(name="allowedchannels", description="See what channels the bot is allowed to be used in")
+    async def get_allowed_channels(self, ctx: commands.Context):
+        response = "You can interact with me in the following channels:\n\n"
+
+        channel_ids = guild_actions.get_allowed_channels(ctx.guild.id)
+        for channel_id in channel_ids:
+            channel = self.bot.get_channel(int(channel_id))
+            if channel is None:
+                guild_actions.remove_allowed_channel(ctx.guild.id, channel_id)
+            else:
+                response += f"{channel.mention}\n"
+
+        await ctx.send(response)
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(ChannelBind(bot))
