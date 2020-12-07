3ae3fcb2a6e293050625d4bf1f313941341447e5
Turner Vink <turner.vink@gmail.com>
2020-12-06 16:39:47 -0800

    Remove print statements, fix bug if cooldown end was None

--- util/database/cooldown_actions.py
+++ util/database/cooldown_actions.py
@@ -42,26 +42,24 @@ def decrease_cooldown_multiplier(user: discord.User):
 
 def update_cooldown_end_time(user: discord.User, appraisal_time: int):
     last_cooldown_end_time = get_cooldown_end_time(user)
-    time_since_cooldown_end = appraisal_time - last_cooldown_end_time
-    print(f"Seconds since last cooldown ended: {time_since_cooldown_end}")
 
-    # If within 5 minutes of last cooldown expiring increase the next cooldown
-    # If between 5 and 10 minutes leave it the same
-    # If more than 10 decrease it
-    if time_since_cooldown_end <= 300:
-        print("Increasing cooldown")
-        increase_cooldown_multiplier(user)
-    elif time_since_cooldown_end > 600:
-        print("Decreasing cooldown")
-        decrease_cooldown_multiplier(user)
+    if last_cooldown_end_time is None:
+        db.reference(f"{db_root}/cooldowns/{user.id}/cooldownEnd").set(int(time.time()) + BASE_COOLDOWN_PERIOD_SECONDS)
     else:
-        print("Maintaining cooldown")
+        time_since_cooldown_end = appraisal_time - last_cooldown_end_time
 
-    cooldown_multiplier = get_cooldown_multiplier(user)
-    cooldown_length = BASE_COOLDOWN_PERIOD_SECONDS * cooldown_multiplier
-    print(f"New cooldown will be {cooldown_length} seconds")
+        # If within 5 minutes of last cooldown expiring increase the next cooldown
+        # If between 5 and 10 minutes leave it the same
+        # If more than 10 decrease it
+        if time_since_cooldown_end <= 300:
+            increase_cooldown_multiplier(user)
+        elif time_since_cooldown_end > 600:
+            decrease_cooldown_multiplier(user)
 
-    db.reference(f"{db_root}/cooldowns/{user.id}/cooldownEnd").set(int(time.time()) + cooldown_length)
+        cooldown_multiplier = get_cooldown_multiplier(user)
+        cooldown_length = BASE_COOLDOWN_PERIOD_SECONDS * cooldown_multiplier
+
+        db.reference(f"{db_root}/cooldowns/{user.id}/cooldownEnd").set(int(time.time()) + cooldown_length)
 
 
 BASE_COOLDOWN_PERIOD_SECONDS = 300
