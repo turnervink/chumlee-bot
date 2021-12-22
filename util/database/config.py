6fcde2970e61c42b173ae650becad5de412d188b
Turner Vink <turner.vink@gmail.com>
2021-12-22 15:06:31 -0800

    Moved some of the DB creds into into env vars so we can run it in Docker

--- util/database/config.py
+++ util/database/config.py
@@ -1,10 +1,15 @@
 import firebase_admin
 from firebase_admin import db, credentials
 
+import json
 import os
 
-cred = credentials.Certificate(os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
-default_app = firebase_admin.initialize_app(cred, {
+db_creds_file = open(os.environ["DB_CREDS_FILE"], "r")
+db_creds = json.loads(db_creds_file.read())
+db_creds["private_key_id"] = os.environ["DB_PRIV_KEY_ID"]
+db_creds["private_key"] = os.environ["DB_PRIV_KEY"]
+
+default_app = firebase_admin.initialize_app(credentials.Certificate(db_creds), {
     "databaseURL": "https://chumlee-bot.firebaseio.com",
     "databaseAuthVariableOverride": {
         "uid": os.environ["DB_AUTH_UID"]
