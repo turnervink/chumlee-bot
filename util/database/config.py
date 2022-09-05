df02d4616325eb8c427d9542bbf18c9b554cd370
Turner Vink <turner.vink@gmail.com>
2022-09-05 16:27:40 -0700

    Reconfigure DB creds again to move back to my own host since Heroku is killing their free tier

--- util/database/config.py
+++ util/database/config.py
@@ -1,15 +1,15 @@
 import firebase_admin
 from firebase_admin import db, credentials
 
-import json
 import os
+from os.path import exists
+import sys
 
-db_creds_file = open(os.environ["DB_CREDS_FILE"], "r")
-db_creds = json.loads(db_creds_file.read())
-db_creds["private_key_id"] = os.environ["DB_PRIV_KEY_ID"]
-db_creds["private_key"] = os.environ["DB_PRIV_KEY"]
+if not exists(os.environ["GOOGLE_APPLICATION_CREDENTIALS"]):
+    print(f"No Firebase credentials file at {os.environ['GOOGLE_APPLICATION_CREDENTIALS']}")
+    sys.exit(1)
 
-default_app = firebase_admin.initialize_app(credentials.Certificate(db_creds), {
+default_app = firebase_admin.initialize_app(credentials.ApplicationDefault(), {
     "databaseURL": "https://chumlee-bot.firebaseio.com",
     "databaseAuthVariableOverride": {
         "uid": os.environ["DB_AUTH_UID"]
