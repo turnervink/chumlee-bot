e0d5bed9bedfddcda239306f5233b20e763a0ca9
Turner Vink <turner.vink@gmail.com>
2022-10-14 20:52:08 -0700

    Add the guilds intent so the bot can get channel names, fixed the channel check breaking if there were no allowed channels

--- util/database/guild_actions.py
+++ util/database/guild_actions.py
@@ -1,13 +1,13 @@
 from .config import db, db_root
 
 
-def add_allowed_channel(guild_id: str, channel_id: str):
+def add_allowed_channel(guild_id, channel_id):
     db.reference(f"{db_root}/guilds/{guild_id}/allowedChannels/{channel_id}").set(True)
 
 
-def remove_allowed_channel(guild_id: str, channel_id: str):
+def remove_allowed_channel(guild_id, channel_id):
     db.reference(f"{db_root}/guilds/{guild_id}/allowedChannels/{channel_id}").delete()
 
 
-def get_allowed_channels(guild_id: str):
+def get_allowed_channels(guild_id):
     return db.reference(f"{db_root}/guilds/{guild_id}/allowedChannels").get()
