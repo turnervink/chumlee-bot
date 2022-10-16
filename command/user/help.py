0db293146d8d3a0fc4b53989dbd2cf5d662a1b30
Turner Vink <turner.vink@gmail.com>
2022-10-15 19:21:15 -0700

    Remove commands command

--- command/user/help.py
+++ command/user/help.py
@@ -38,17 +38,6 @@ class Help(commands.Cog):
         await ctx.defer()
         await ctx.respond(embed=self.welcome_embed)
 
-    @commands.command(name="commands", description="List all commands", usage="commands")
-    async def commands(self, ctx):
-        async with ctx.typing():
-            embed = discord.Embed(title="Commands", colour=discord.Colour(0xffffff))
-            for command in self.bot.commands:
-                if not command.hidden:
-                    embed.add_field(name=f"{self.bot.command_prefix}{command.usage}",
-                                    value=f"{command.description}", inline=False)
-
-            await ctx.send(embed=embed)
-
 
 def setup(bot):
     bot.remove_command("help")
