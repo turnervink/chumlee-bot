78e8eebc02a725a4f420b6ba7031709c6da65dfb
Turner Vink <turner.vink@gmail.com>
2022-10-15 19:12:00 -0700

    Convert pay command

--- command/user/usertransaction.py
+++ command/user/usertransaction.py
@@ -1,27 +1,30 @@
 import discord
-from discord.ext import commands
+from discord.ext import bridge, commands
 
 from command.check import checks
-from util.database import transaction_actions, user_actions
-from util import emoji
 from error import errors
+from util import emoji
+from util.database import transaction_actions, user_actions
 
 
 class UserTransaction(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="pay", description="Pay another user some Chumcoins", usage="pay <@user> <amount>")
+    @commands.slash_command(name="pay", description="Pay another user some Chumcoins", usage="pay <@user> <amount>")
     @checks.user_registered()
-    async def pay(self, ctx, payee: discord.User, amount: int):
+    async def pay(self, ctx: bridge.BridgeApplicationContext, payee: discord.User, amount: int):
         async with ctx.typing():
-            payer = ctx.message.author
+            payer = ctx.author
 
             if payer == payee:
                 raise errors.TransactionUsersAreEqualError(payer)
 
+            if payee == self.bot.user:
+                raise commands.BadArgument(f"I appreciate it, {payer.mention}, but I can't take your {emoji.CHUMCOIN}s!")
+
             if not user_actions.is_registered(payee):
-                raise errors.UserNotRegisteredError(payee)
+                raise errors.UserNotFoundError(payee)
 
             if amount <= 0:
                 raise commands.BadArgument("You can't pay someone a value that's zero or less!")
@@ -29,7 +32,7 @@ class UserTransaction(commands.Cog):
             transaction_actions.withdraw(payer, amount)
             transaction_actions.deposit(payee, amount)
 
-            await ctx.send(f"{ctx.message.author.mention} {emoji.ARROW_RIGHT} {amount} {emoji.CHUMCOIN} "
+            await ctx.respond(f"{ctx.author.mention} {emoji.ARROW_RIGHT} {amount} {emoji.CHUMCOIN} "
                            f"{emoji.ARROW_RIGHT} {payee.mention}")
 
 
