69a92bed7490e8cf4beef383d1772b0f9073085d
Turner Vink <turner.vink@gmail.com>
2022-10-15 17:57:27 -0700

    Convert medals commands

--- command/pawnshop/medal.py
+++ command/pawnshop/medal.py
@@ -1,4 +1,5 @@
-from discord.ext import commands
+import discord
+from discord.ext import bridge, commands
 
 from util import emoji
 from util.database import user_actions, transaction_actions
@@ -9,33 +10,37 @@ class Medal(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="listmedals", aliases=["medals"], description="See a list of available Chummedals",
-                      usage="listmedals")
-    async def list_medals(self, ctx: commands.Context):
-        async with ctx.typing():
-            response = ""
+    @commands.slash_command(name="medals", description="See a list of available Chummedals",
+                      usage="medals")
+    async def list_medals(self, ctx: bridge.BridgeApplicationContext):
+        await ctx.defer()
+        response = ""
 
-            for medal in MedalObject.VALID_MEDALS:
-                response += f"{medal.title()} Chummedal - {MedalObject.MEDAL_DATA[medal]['price']} {emoji.CHUMCOIN}\n"
+        for medal in MedalObject.VALID_MEDALS:
+            response += f"{medal.title()} Chummedal - {MedalObject.MEDAL_DATA[medal]['price']} {emoji.CHUMCOIN}\n"
 
-            await ctx.send(response)
+        await ctx.respond(response)
 
-    @commands.command(name="buymedal", description="Buy a Chummedal", usage="buymedal <medal name>")
-    async def buy_medal(self, ctx: commands.Context, medal_name: str):
-        async with ctx.typing():
-            medal = MedalObject(medal_name.lower())
-            user_medals = user_actions.get_medals(ctx.message.author)
+    @commands.slash_command(name="buymedal", description="Buy a Chummedal", usage="buymedal <medal name>")
+    async def buy_medal(
+            self,
+            ctx: bridge.BridgeApplicationContext,
+            medal_name: discord.Option(str, choices=MedalObject.VALID_MEDALS)
+    ):
+        await ctx.defer()
+        medal = MedalObject(medal_name.lower())
+        user_medals = user_actions.get_medals(ctx.author)
 
-            if user_medals is None or medal.name not in user_medals:
-                transaction_actions.withdraw(ctx.message.author, medal.price)
-                user_actions.award_medal(ctx.message.author, medal)
-                response = (f"Awesome! Here's your {medal.name} Chummedal!"
-                            "\n\n"
-                            f"{ctx.message.author.mention} {emoji.ARROW_RIGHT} {medal.price} "
-                            f"{emoji.CHUMCOIN} {emoji.ARROW_RIGHT} {emoji.CHUMLEE}")
-                await ctx.send(response)
-            else:
-                await ctx.send(f"Looks like you've already got the {medal.name} Chummedal {ctx.message.author.mention}!")
+        if user_medals is None or medal.name not in user_medals:
+            transaction_actions.withdraw(ctx.author, medal.price)
+            user_actions.award_medal(ctx.author, medal)
+            response = (f"Awesome! Here's your {medal.name} Chummedal!"
+                        "\n\n"
+                        f"{ctx.author.mention} {emoji.ARROW_RIGHT} {medal.price} "
+                        f"{emoji.CHUMCOIN} {emoji.ARROW_RIGHT} {emoji.CHUMLEE}")
+            await ctx.respond(response)
+        else:
+            await ctx.respond(f"Looks like you've already got the {medal.name} Chummedal {ctx.author.mention}!")
 
 
 def setup(bot: commands.Bot):
